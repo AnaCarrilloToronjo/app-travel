cb9e0ad719c41d1b873614c7552b18027e7fb83b
jkmrto <juanka1331@gmail.com>
2022-05-08 22:59:49 +0200

    setup docker compose

--- api/database/database.js
+++ api/database/database.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost/visits";
+const db_host = process.env.DB_HOST || "localhost";
+console.log("DB_HOST:", db_host);
+
+const URI = `mongodb://${db_host}/visits`;
 
 mongoose
   .connect(URI, { useNewUrlParser: true })
-  .then(db => console.log("DB is connected"))
-  .catch(err => console.error(err));
+  .then((db) => console.log("DB is connected"))
+  .catch((err) => console.error(err));
 
 module.exports = mongoose;
---
+++ client/webpack.prod.js
@@ -0,0 +1,51 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  entry: {
+    app: "./index.tsx",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
+    }),
+
+    new CleanWebpackPlugin(),
+    new Dotenv({
+      path: "./prod.env",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
