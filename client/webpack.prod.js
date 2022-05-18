df75e5057aa3979044c28178a535b5fa2e172e2d
Ana Carrillo <ana.carrillo.toronjo@gmail.com>
2022-05-18 18:41:54 +0200

    wip

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -3,6 +3,8 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const Dotenv = require("dotenv-webpack");
 
+const path = require("path");
+
 module.exports = merge(common, {
   mode: "production",
   module: {
@@ -10,7 +12,8 @@ module.exports = merge(common, {
       {
         test: /\.scss$/,
         exclude: /node_module/,
-        use: [
+        use: ["style-loader", "css-loader", "sass-loader"],
+        /* use: [
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
@@ -21,7 +24,7 @@ module.exports = merge(common, {
             },
           },
           "sass-loader",
-        ],
+        ],*/
       },
     ],
   },
