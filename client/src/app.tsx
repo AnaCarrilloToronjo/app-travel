7c3080cc57c9e5ec8177ed14521502307d5de512
Ana Carrillo <ana.carrillo.toronjo@gmail.com>
2022-05-21 22:08:40 +0200

    fix app client

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,19 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { AppRouter } from "./routers/appRouter";
 
 export const App = () => {
-  const [state, setState] = useState({ apiResponse: "" });
-
-  useEffect(() => {
-    fetch("http://localhost/api/stored")
-      .then((res) => res.text())
-      .then((res) => setState({ apiResponse: res }));
-  }, []);
-
   return (
     <div>
       <AppRouter />
-      {/* <p className="App-intro">;{state.apiResponse}</p>*/}
     </div>
   );
 };
